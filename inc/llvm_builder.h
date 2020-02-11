020dbab116cfdd51e586153a938cba25011fda52
Marijn Stollenga <mstollenga@gmail.com>
2020-02-11 01:10:10 +0100

    Starting structs and arrays

--- inc/llvm_builder.h
+++ inc/llvm_builder.h
@@ -90,6 +90,8 @@ struct ExpBuilder : NodeBuilder {
     register_callback("loadvarptrcreate", std::bind(&ExpBuilder::p_loadvarptrcreate, this, _1));
     register_callback("loadvarptr", std::bind(&ExpBuilder::p_loadvarptr, this, _1));
     register_callback("loadvar", std::bind(&ExpBuilder::p_loadvar, this, _1));
+    register_callback("getelement", std::bind(&ExpBuilder::p_getelement, this, _1));
+    register_callback("getelementptr", std::bind(&ExpBuilder::p_getelement, this, _1));
     register_callback("stat", std::bind(&ExpBuilder::p_statement, this, _1));
     register_callback("ret", std::bind(&ExpBuilder::p_return, this, _1));
     register_callback("inc", std::bind(&ExpBuilder::p_inc, this, _1));
@@ -181,6 +183,44 @@ struct ExpBuilder : NodeBuilder {
     }
     value_vector[n] = builder->CreateLoad(value_ptr, false);
   }
+
+    void p_getelement(int n) {
+        SearchNode node{n, pg};
+        auto value_ptr = llvm_value(node.child().N);
+        builder->CreateLoad(value_ptr, false);
+    }
+
+    void p_getelementptr(int n) {
+        SearchNode node{n, pg};
+        auto load_ptr = node.child(0);
+        auto load_element = node.child(1);
+        
+        auto value_ptr = llvm_value(load_ptr.N);
+
+        auto elements = load_element.get_all("element");
+        for (auto n : elements) {
+            auto value_type = value_ptr->getType();
+            auto child_node = n.child();
+            if (child_node.type() == "name") {
+                if (value_type->isStructTy()) {
+                    auto name = value_type->getStructName();
+                    auto variable_index = llvm::ConstantInt::get(llvm::Type::getInt64Ty(C), 0);
+                    value_ptr = builder->CreateGEP(value_ptr, std::vector<llvm::Value*>{variable_index});
+                    
+                } else {
+                    throw std::runtime_error("can't find variable, base type is not a struct");
+                }
+            }
+            if (child_node.type() == "number") {
+                if (value_type->isPointerTy() || value_type->isArrayTy()) {
+                    value_ptr = builder->CreateGEP(value_ptr, std::vector<llvm::Value*>{llvm_value(child_node.N)});
+                } else {
+                    throw std::runtime_error("Variable not a ptr or array type");
+                }
+            }
+        }
+        value_vector[node.N] = value_ptr;
+    }
   
   void p_statement(int n) {
     SearchNode node{n, pg};
