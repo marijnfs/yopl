e5265c66379d42d7c05656373021be9b19a00607
Marijn Stollenga <m.stollenga@gmail.com>
2020-03-08 23:13:34 +0100

    fix member/index calculation

--- src/llvm_builder.cc
+++ src/llvm_builder.cc
@@ -31,14 +31,17 @@ void ExpBuilder::p_getelementptr(int n) {
     for (auto n : elements) {
         auto value_type = value_ptr->getType();
         auto child_node = n.child();
-        if (child_node.type() == "name") {
+        if (child_node.type() == "member") {
+            std::cout << "member: " << child_node.text() << std::endl;
             auto struct_name = value_type->getPointerElementType()->getStructName();
             auto struct_member = child_node.text();
             auto index = context->get_struct_member_index(struct_name, struct_member);
             auto index_constant = llvm::ConstantInt::get(llvm::Type::getInt64Ty(C), index);
             value_ptr = builder->CreateGEP(value_ptr, std::vector<llvm::Value*>{index_constant});
         }
-        if (child_node.type() == "number") {
+        if (child_node.type() == "index") {
+            std::cout << "index: " << child_node.text() << std::endl;
+
             value_ptr = builder->CreateGEP(value_ptr, std::vector<llvm::Value*>{llvm_value(child_node.N)});
         }
         if (child_node.type() == "ptr") {
